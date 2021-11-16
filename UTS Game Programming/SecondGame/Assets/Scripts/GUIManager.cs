24871d7f3d7311f734e9f426ee8c20d3d3b9cd06
bgkoro <putrabagaskoro46@gmail.com>
2021-11-16 12:03:03 +0700

    Tambahkan
    commit

---
+++ UTS Game Programming/SecondGame/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPlay() {
+        SceneManager.LoadScene("Main");
+    }
+    public void OnCredit() {
+        SceneManager.LoadScene("Credit");
+    }
+    public void OnHelp() {
+        SceneManager.LoadScene("Help");
+    }
+    public void OnMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UTS Game Programming/SecondGame/Assets/Scripts/Perilaku.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Perilaku : MonoBehaviour
+{
+    
+
+    void OnCollisionEnter2D(Collision2D coll){
+if (coll.gameObject.tag == "enemyTag"){
+    Debug.Log("Game Over");
+    Die();
+    }
+else if(coll.gameObject.tag == "finishTag"){
+    Debug.Log("FINISHH !!!");
+    Finish();
+    }
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Die(){
+        Debug.Log("GAME OVER");
+        SceneManager.LoadScene ("Gameover");
+    }
+    void Finish(){
+        Debug.Log("Finish");
+        SceneManager.LoadScene ("Finish");
+    }
+}
