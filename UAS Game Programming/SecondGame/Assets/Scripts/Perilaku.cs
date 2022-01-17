966d854b55d45cdcaf81202f39fd6a58cfec6b49
bgkoro <42777725+bgkoro@users.noreply.github.com>
2022-01-17 22:46:03 +0700

    fileuas
    fileuas

---
+++ UAS Game Programming/SecondGame/Assets/Scripts/Perilaku.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Perilaku : MonoBehaviour
+{
+    
+    public Text PowerupText;
+    private string PowerNum;
+
+    void OnCollisionEnter2D(Collision2D coll){
+if (coll.gameObject.tag == "enemyTag"){
+     if(PowerNum == "Off"){
+        Debug.Log("Game Over");
+        Die();
+     }else{
+
+     }
+    }
+else if(coll.gameObject.tag == "finishTag"){
+    Debug.Log("FINISHH !!!");
+    Finish();
+    }
+}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        PowerNum = "Off";
+        PowerupText.text = "Power Up : " +PowerNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Die(){
+        Debug.Log("GAME OVER");
+        SceneManager.LoadScene ("Gameover");
+    }
+    void Finish(){
+        Debug.Log("Finish");
+        SceneManager.LoadScene ("Finish");
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D Powerup){
+
+        if(Powerup.tag =="powerupTag"){
+
+        PowerNum = "On";
+        Destroy(Powerup.gameObject);
+        PowerupText.text = "Power Up : " +PowerNum;
+
+        }
+    }
+}
